08a45c3bad37bef2540519d53f1c8e3edb51921f
hefnrh <516860685@qq.com>
2013-11-24 23:20:54 +0800

    修改为标签页形式

--- src/com/hefnrh/appformcs/MainActivity.java
+++ src/com/hefnrh/appformcs/MainActivity.java
@@ -13,10 +13,11 @@ import android.view.View;
 import android.widget.AdapterView;
 import android.widget.Button;
 import android.widget.Spinner;
+import android.widget.TabHost;
 import android.widget.TextView;
 
 public class MainActivity extends Activity {
-	
+
 	private TextView status;
 	private TextView result;
 	private Button run;
@@ -26,28 +27,42 @@ public class MainActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-		
+
+		setTitle("IS MATH 2 DEMO");
+
 		status = (TextView) findViewById(R.id.textView1);
 		result = (TextView) findViewById(R.id.textView2);
 		run = (Button) findViewById(R.id.button1);
-		
-		String[] items = {"1 thread", "2 threads", "4 threads", "6 threads", "8 threads"};
+
+		String[] items = { "1 thread", "2 threads", "4 threads", "6 threads",
+				"8 threads" };
 		MyAdapter adapter = new MyAdapter(this, items);
-		
+
 		multiChoice = (Spinner) findViewById(R.id.spinner1);
 		multiChoice.setAdapter(adapter);
-		multiChoice.setOnItemSelectedListener(new Spinner.OnItemSelectedListener() {
-			@Override
-			public void onItemSelected(AdapterView<?> arg0, View arg1, int arg2,
-					long arg3) {
-				// TODO Auto-generated method stub
-			}
-			@Override
-			public void onNothingSelected(AdapterView<?> arg0) {
-				// TODO Auto-generated method stub
-				
-			}
-		});
+		multiChoice
+				.setOnItemSelectedListener(new Spinner.OnItemSelectedListener() {
+					@Override
+					public void onItemSelected(AdapterView<?> arg0, View arg1,
+							int arg2, long arg3) {
+					}
+
+					@Override
+					public void onNothingSelected(AdapterView<?> arg0) {
+					}
+				});
+
+		TabHost tabHost = (TabHost) findViewById(android.R.id.tabhost);
+		tabHost.setup();
+		TabHost.TabSpec spec = tabHost.newTabSpec("multiply");
+		spec.setContent(R.id.multiplylayout);
+		spec.setIndicator("10W multiply");
+		tabHost.addTab(spec);
+
+		spec = tabHost.newTabSpec("mcs");
+		spec.setContent(R.id.MCSLayout);
+		spec.setIndicator("MCS");
+		tabHost.addTab(spec);
 	}
 
 	@Override
@@ -56,54 +71,42 @@ public class MainActivity extends Activity {
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
 	}
-	
+
 	@Override
 	public boolean onOptionsItemSelected(MenuItem item) {
 		switch (item.getItemId()) {
-		case R.id.item1: finish(); break;
-		default: break;
+		case R.id.item1:
+			finish();
+			break;
+		default:
+			break;
 		}
 		return true;
 	}
+
 	public void exit(View v) {
 		finish();
 	}
-	
+
 	public void run(View v) {
 		run.setEnabled(false);
 		int n = multiChoice.getSelectedItemPosition();
 		status.setText("status: running");
 		long res;
-		if (n == 0) {
-			res = singleThreadTest();
+		if (n == 0)
 			n = 1;
-		} else {
+		else
 			n <<= 1;
-			res = multiThreadTest(n);
-		}
-		result.setText("time used: " + res + " ms with " + n + " threads");
+		res = multiThreadTest(n);
+		if (res >= 0)
+			result.setText("time used: " + res + " ms with " + n + " threads");
 		status.setText("status: run over");
 		run.setEnabled(true);
 	}
-	
-	private long singleThreadTest() {
-		byte b = 14; // 00001110
-		b |= -128;	 // 10001110
-		byte p = 14; //	00001110
-		p |= -128;	 // 10001110 x^8 + x^4 + x^3 + x^2 + 1
-		byte minus1 = -1;
-		Field28 field = new Field28(p);
-		long start = System.currentTimeMillis();
-		for (int i = 0; i < 100000; ++i) {
-			b = field.multiply(b, minus1);
-		}
-		long end = System.currentTimeMillis();
-		return end - start;
-	}
-	
+
 	private long multiThreadTest(int n) {
 		byte p = 14; // 00001110
-		p |= -128;   // 10001110 x^8 + x^4 + x^3 + x^2 + 1
+		p |= -128; // 10001110 x^8 + x^4 + x^3 + x^2 + 1
 		Field28 field = new Field28(p);
 		Counter c = new Counter();
 		CountDownLatch cdl = new CountDownLatch(n);
@@ -116,14 +119,10 @@ public class MainActivity extends Activity {
 		try {
 			cdl.await();
 		} catch (InterruptedException e) {
-			status.setText("an error occurred when running");
-			return 0;
+			result.setText("an error occurred when running");
+			return -1;
 		}
 		return c.getTime();
-	}
-	
-	public void change10W2MCS(View v) {
-		// TODO
 	}
 
 }
